daa27141af5cd9c17f676764830d815f5889ec80
React-Profi <vsesvitarchos@gmail.com>
2025-05-02 15:43:50 +0100

    фикс в eslint не работал

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,10 +1,15 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 import baseConfig from '../eslint.config.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default [
 	...baseConfig,
 	{
+		files: ['src/**/*.{ts,tsx,js,jsx}'],
 		languageOptions: {
 			parserOptions: {
 				project: [path.resolve(__dirname, './tsconfig.json')],
